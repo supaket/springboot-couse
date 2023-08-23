85b746c18ef3511994b796cd2eabb5f4da380acc
supaket <supaket@enersys.co.th>
2023-08-23 12:54:22 +0700

    Add lottery class

--- src/main/java/com/example/demo/LotteryService.java
+++ src/main/java/com/example/demo/LotteryService.java
@@ -1,16 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
+@Component
 public class LotteryService {
-    public void sayHi() {
-        System.out.println("Called sayHi");
+    public void lotto() {
+        System.out.println(new Random().nextInt(100));
     }
 }
 
 @Component
 class MainComponent {
+
+    public LotteryService service;
+
     public void sayHelloFunction() {
         System.out.println("Hello");
+        service.lotto();
     }
 }
